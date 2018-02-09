34ff827fc7e4e674834c40a69b727273963d6298
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-08 23:47:30 -0800

    add alternate solution

--- selfDividingNumbers.js
+++ selfDividingNumbers.js
@@ -45,3 +45,30 @@ const selfDividingNumbers = (left, right) => {
 
   return validNumsArr
 }
+
+// ALTERNATE SOLUTION =======
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+const selfDividingNumbers = (left, right) => {
+  let numStr
+  const validNumsArr = []
+
+  while (left <= right) {
+    numStr = left.toString()
+    if (numStr.indexOf('0') !== -1) {
+      left++
+      continue
+    }
+
+    if (numStr.split('').filter(digit => left % +digit === 0).length === numStr.length) {
+      validNumsArr.push(left)
+    }
+    left++
+  }
+
+  return validNumsArr
+}
