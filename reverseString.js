0ed01a0de425d6b40694772b1475073dadb255d2
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-16 17:25:19 -0800

    set up Reverse String empty prompt

---
+++ reverseString.js
@@ -0,0 +1,20 @@
+// 344. Reverse String
+
+/* PROMPT ===================
+
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+
+*/
+
+// SOLUTION =================
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseString = (s) => {
+
+}
