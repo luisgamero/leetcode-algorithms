10ccb13c47fc23d11d9405a63c7d9985a88d2f2c
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-16 17:31:02 -0800

    complete solution

--- reverseString.js
+++ reverseString.js
@@ -16,5 +16,11 @@ Given s = "hello", return "olleh".
  * @return {string}
  */
 const reverseString = (s) => {
+  let reversed = ''
 
+  for (let i = s.length - 1; i >= 0; i--) {
+    reversed += s[i]
+  }
+
+  return reversed
 }
