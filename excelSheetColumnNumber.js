4f1c8779df6b6160e0f59c5df9e1c635a5217282
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-22 22:09:42 -0800

    set up prompt

---
+++ excelSheetColumnNumber.js
@@ -0,0 +1,27 @@
+// 171. Excel Sheet Column Number
+
+/* PROMPT ===================
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+  A -> 1
+  B -> 2
+  C -> 3
+  ...
+  Z -> 26
+  AA -> 27
+  AB -> 28
+
+*/
+
+// SOLUTION =================
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const titleToNumber = (s) => {
+
+}
