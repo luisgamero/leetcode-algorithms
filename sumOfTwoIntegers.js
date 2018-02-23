e4a99a644fb8d451bba376dbacb24bc8e67cd1e4
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-22 21:46:24 -0800

    set up prompt

---
+++ sumOfTwoIntegers.js
@@ -0,0 +1,21 @@
+// 371. Sum of Two Integers
+
+/* PROMPT ===================
+
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+  Given a = 1 and b = 2, return 3.
+
+*/
+
+// SOLUTION =================
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+const getSum = (a, b) => {
+
+}
