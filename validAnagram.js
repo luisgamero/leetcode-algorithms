0f59bad58d948f580e06d822aedc779f4e699b5d
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-25 14:23:36 -0800

    set up prompt

---
+++ validAnagram.js
@@ -0,0 +1,25 @@
+// 242. Valid Anagram
+
+/* PROMPT ===================
+
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+  You may assume the string contains only lowercase alphabets.
+
+*/
+
+// SOLUTION =================
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) => {
+
+}
