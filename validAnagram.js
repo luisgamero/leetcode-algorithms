dd5ea7b51f3433a9329f66629a824c2e11bd9f4f
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-25 14:38:47 -0800

    complete solution

--- validAnagram.js
+++ validAnagram.js
@@ -21,5 +21,14 @@ Note:
  * @return {boolean}
  */
 const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false
 
+  const sortedS = s.split('').sort()
+  const sortedT = t.split('').sort()
+
+  for (let i = 0; i < s.length; i++) {
+    if (sortedS[i] !== sortedT[i]) return false
+  }
+
+  return true
 }
