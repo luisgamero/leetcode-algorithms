343494df11fe9b35d62fb2db10712644ee9c4a63
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-26 15:40:53 -0800

    set up prompt

---
+++ containsDuplicate.js
@@ -0,0 +1,17 @@
+// 217. Contains Duplicate
+
+/* PROMPT ===================
+
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+*/
+
+// SOLUTION =================
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+
+}
