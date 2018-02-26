4cb174cbde62a038c29b8179e8cb04b345e53896
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-26 15:44:01 -0800

    complete solution

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -13,5 +13,11 @@ Given an array of integers, find if the array contains any duplicates. Your func
  * @return {boolean}
  */
 const containsDuplicate = (nums) => {
+  const map = {}
 
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] === true) return true
+    map[nums[i]] = true
+  }
+  return false
 }
