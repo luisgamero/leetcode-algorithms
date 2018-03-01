dfcd0b8913a1a9d58cc20c086970472fb90e4149
Luis Gamero <gamero.luis.enrique@gmail.com>
2018-02-28 17:52:00 -0800

    complete solution

--- deleteNodeInALinkedList.js
+++ deleteNodeInALinkedList.js
@@ -22,5 +22,6 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
  * @return {void} Do not return anything, modify node in-place instead.
  */
 const deleteNode = (node) => {
-
+  node.val = node.next.val
+  node.next = node.next.next
 }
